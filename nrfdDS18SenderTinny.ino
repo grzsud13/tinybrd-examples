9790f6044d7fcbad2a463c5f30bb1cc77f9c5b81
Witold Rugowski <rugowski@nhw.pl>
2015-02-15 12:30:56 +0100

    Power save w oddzielnym pliku

--- nrfdDS18SenderTinny.ino
+++ nrfdDS18SenderTinny.ino
@@ -1,5 +1,3 @@
-#include <avr/sleep.h> //Needed for sleep_mode
-#include <avr/wdt.h> //Needed to enable/disable watch dog timer
 
 #include <SPI85.h>
 #include <Mirf.h>
@@ -21,9 +19,6 @@ DS18B20 sensor(&onewire);
 
 
 
-ISR(WDT_vect) {
-//Don't do anything. This is just here so that we wake up.
-}
 
 
 void setup()
@@ -40,33 +35,9 @@ void setup()
   sensor.begin();
   sensor.request(address);
   
-   set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Power down everything, wake up from WDT
-sleep_enable();
-}
- //Sets the watchdog timer to wake us up, but not reset
-//0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms
-//6=1sec, 7=2sec, 8=4sec, 9=8sec
-//From: http://interface.khm.de/index.php/lab/experiments/sleep_watchdog_battery/
-void setup_watchdog(int timerPrescaler) {
-if (timerPrescaler > 9 ) timerPrescaler = 9; //Limit incoming amount to legal settings
-byte bb = timerPrescaler & 7;
-if (timerPrescaler > 7) bb |= (1<<5); //Set the special 5th bit if necessary
-//This order of commands is important and cannot be combined
-MCUSR &= ~(1<<WDRF); //Clear the watch dog reset
-WDTCSR |= (1<<WDCE) | (1<<WDE); //Set WD_change enable, set WD enable
-WDTCSR = bb; //Set new watchdog timeout value
-WDTCSR |= _BV(WDIE); //Set the interrupt enable, this will keep unit from resetting after each int
-}
+  power_setup();
 
- 
- void pulseLED(){
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);  
-  delay(250);
 }
-
-float heartbeat = 64;
 void loop()
 {
   
@@ -83,13 +54,15 @@ if (sensor.available())
   }
 //  delay(300);
 
+  power_down();
+  /*
     ADCSRA &= ~(1<<ADEN); //Disable ADC, saves ~230uA
     digitalWrite(CE, LOW);
     for (byte i=0; i<4; i++) {
       setup_watchdog(4); //Setup watchdog to go off after 8sec
       sleep_mode(); //Go to sleep! Wake up 1sec later and check water
     }  
-      
+      */
     ADCSRA |= (1<<ADEN); //Enable ADC
    digitalWrite(CE, HIGH);
    
