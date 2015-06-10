87cdec18ef7749f195e386f28c93c83298c80cf1
Witold Rugowski <rugowski@nhw.pl>
2015-06-10 12:44:45 +0200

    release skrypty

---
+++ do-release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm -f *.zip
+
+mkdir tmp
+mkdir tmp/tinyBrdExamples
+
+zip -r tmp/TSM.zip tinyBrdTemp ReceiverUNO tinyBrdLight
+cd tmp/tinyBrdExamples
+unzip ../TSM
+cd ..
+zip -r ../tinyBrdExamples.zip tinyBrdExamples
+cd ..
+rm -fR tmp
+
